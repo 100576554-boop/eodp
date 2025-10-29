c79312c5b25736316c3ac1e560e3120ce3b133f1
100576554-boop <100576554@alumnos.uc3m.es>
2025-10-29 23:36:19 +0100

    mainL1b, ISM1as input, L1b as ouput

--- l1b/mainL1b.py
+++ l1b/mainL1b.py
@@ -5,8 +5,8 @@ from l1b.src.l1b import l1b
 
 # Directory - this is the common directory for the execution of the E2E, all modules
 auxdir = r'C:\\Users\\ciroa\\Desktop\\UNI\\Erasmus\\EODP\\EODP_code\\eodp\\auxiliary'
-indir = r"C:\\Users\\ciroa\\Desktop\\UNI\\Erasmus\\EODP\\EODP-TS-L1B\\input"
-outdir = r"C:\\Users\\ciroa\\Desktop\\UNI\\Erasmus\\EODP\\MyOutputFALSE_EODP"
+indir = r"C:\\Users\\ciroa\\Desktop\\UNI\\Erasmus\\EODP\\MyOutputs_ISM1_EODP"
+outdir = r"C:\\Users\\ciroa\\Desktop\\UNI\\Erasmus\\EODP\\MyOutputL1B_16_10_EODP"
 
 # Initialise the ISM
 myL1b = l1b(auxdir, indir, outdir)
