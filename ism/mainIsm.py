a4991f2cf0c2c11d1610ae5aac65d6452c1d537b
100576554-boop <100576554@alumnos.uc3m.es>
2025-10-29 23:35:49 +0100

    mainIsm, sgm_out is input, ISM1 output

--- ism/mainIsm.py
+++ ism/mainIsm.py
@@ -5,8 +5,8 @@ from ism.src.ism import ism
 
 # Directory - this is the common directory for the execution of the E2E, all modules
 auxdir = r'C:\\Users\\ciroa\\Desktop\\UNI\\Erasmus\\EODP\\EODP_code\\eodp\\auxiliary'
-indir = r"C:\Users\ciroa\Desktop\UNI\Erasmus\EODP\EODP-TS-ISM\input\gradient_alt100_act150" #small scene
-outdir = r"C:\\Users\\ciroa\\Desktop\\UNI\\Erasmus\\EODP\\MyOutputs_ISM_EODP"
+indir = r"C:\Users\ciroa\Desktop\UNI\Erasmus\EODP\EODP_TER_2021\EODP-TS-E2E\sgm_out" #small scene
+outdir = r"C:\\Users\\ciroa\\Desktop\\UNI\\Erasmus\\EODP\\MyOutputs_ISM1_EODP"
 
 # Initialise the ISM
 myIsm = ism(auxdir, indir, outdir)
