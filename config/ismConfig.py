fd8322a77a8721d31e8cb856e0537736c535018d
100576554-boop <100576554@alumnos.uc3m.es>
2025-11-02 00:19:40 +0100

    configuration ISM-0001

--- config/ismConfig.py
+++ config/ismConfig.py
@@ -9,16 +9,16 @@ class ismConfig:
         # Configuration parameters
         #--------------------------------------------------------------------------------
         # CCD
-        #self.pix_size = 30e-6                    # [m] Pixel size in microns for the MS channels
-        #self.t_int = 0.00672                     # [s] Integration time
-        self.pix_size = 42e-6
-        self.t_int = 0.0428
+        self.pix_size = 30e-6                    # [m] Pixel size in microns for the MS channels
+        self.t_int = 0.00672                     # [s] Integration time
+        #self.pix_size = 42e-6
+        #self.t_int = 0.0428
 
         # Optical system
-        #self.D = 0.150
-        self.D = 0.0765                          # [m] Telescope pupil diameter
-        #self.f = 0.5262                          # [m] Focal length
-        self.f = 0.2345
+        self.D = 0.150
+        #self.D = 0.0765                          # [m] Telescope pupil diameter
+        self.f = 0.5262                          # [m] Focal length
+        #self.f = 0.2345
         self.Tr = 0.99                           # [-] Optical transmittance
         self.wLF = 100e-9                        # [m] RMS of low-frequency wavefront errors
         self.wHF = 100e-9                        # [m] RMS of high-frequency wavefront errors
@@ -79,6 +79,6 @@ class ismConfig:
         # Optical stage. Use the PSF convolution. If False, will use the MTF
         self.do_psf_conv = False
         # Detection stage errors and effects
-        self.apply_prnu = False #prima erano true questi sotto
-        self.apply_dark_signal = False
-        self.apply_bad_dead = False
+        self.apply_prnu = True #prima erano true questi sotto
+        self.apply_dark_signal = True
+        self.apply_bad_dead = True
